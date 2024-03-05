53151fad458be25c5876438b69ed323a71449f56
frida <frida.valecchi@gmail.com>
2024-03-05 08:51:21 +0100

    primo commit

---
+++ src/main/java/com/mycompany/git1/GIT1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.git1;
+
+/**
+ *
+ * @author frida
+ */
+public class GIT1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
